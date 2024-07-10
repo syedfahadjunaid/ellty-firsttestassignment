2d15b14cfac531164ba3b61cc8fb6968c1af5b86
Syed Fahad Junaid <syedfahadjunaid@gmail.com>
2024-07-10 11:45:43 +0530

    commit

--- src/components/CheckBox/Checkbox.js
+++ src/components/CheckBox/Checkbox.js
@@ -1,45 +1,80 @@
-import * as React from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Checkbox from "@mui/material/Checkbox";
+// import * as React from "react";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import Checkbox from "@mui/material/Checkbox";
 
-export default function Checkboxes({
-  disabled,
-  color,
-  checkboxBorder,
-  checked,
-  setChecked,
-}) {
-  const theme = createTheme({
-    components: {
-      MuiCheckbox: {
-        styleOverrides: {
-          root: {
-            color: checkboxBorder,
-            "&.Mui-checked": {
-              color: color,
-            },
-          },
-        },
-      },
-    },
-  });
+// export default function Checkboxes({
+//   disabled,
+//   color,
+//   checkboxBorder,
+//   checked,
+//   setChecked,
+// }) {
+//   const theme = createTheme({
+//     components: {
+//       MuiCheckbox: {
+//         styleOverrides: {
+//           root: {
+//             color: checkboxBorder,
+//             "&.Mui-checked": {
+//               color: color,
+//             },
+//           },
+//         },
+//       },
+//     },
+//   });
+//   return (
+//     <div>
+//       <ThemeProvider theme={theme}>
+//         {setChecked ? (
+//           <Checkbox
+//             disabled={disabled}
+//             checked={checked}
+//             onChange={(e) => setChecked(e.target.checked)}
+//           />
+//         ) : (
+//           <Checkbox
+//             disabled={disabled}
+//             defaultChecked={checked}
+//             // onChange={(e) => setChecked && setChecked(e.target.checked)}
+//           />
+//         )}
+//       </ThemeProvider>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import "./Checkbox.css";
+
+const Checkbox = ({ disabled, color, checkboxBorder, checked, setChecked }) => {
   return (
-    <div>
-      <ThemeProvider theme={theme}>
-        {setChecked ? (
-          <Checkbox
-            disabled={disabled}
-            checked={checked}
-            onChange={(e) => setChecked(e.target.checked)}
-          />
-        ) : (
-          <Checkbox
-            disabled={disabled}
-            defaultChecked={checked}
-            // onChange={(e) => setChecked && setChecked(e.target.checked)}
-          />
-        )}
-      </ThemeProvider>
-    </div>
+    <>
+      {setChecked ? (
+        <input
+          type="checkbox"
+          className={`w-[25px] h-[25px]`}
+          checked={checked}
+          disabled={disabled}
+          style={{
+            accentColor: color,
+          }}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+      ) : (
+        <input
+          type="checkbox"
+          className={`w-[25px] h-[25px]`}
+          // checked={checked}
+          defaultChecked={checked}
+          disabled={disabled}
+          style={{
+            accentColor: color,
+          }}
+        />
+      )}
+    </>
   );
-}
+};
+
+export default Checkbox;
